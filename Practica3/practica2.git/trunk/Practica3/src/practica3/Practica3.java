d50b0132bca2db904b593ab8da1bae07b4ff1bb6
ErrorM <49177787+ErrorM@users.noreply.github.com>
2019-04-02 17:00:33 -0600

    Add files via upload

--- Practica3/practica2.git/trunk/Practica3/src/practica3/Practica3.java
+++ Practica3/practica2.git/trunk/Practica3/src/practica3/Practica3.java
@@ -15,7 +15,8 @@ public class Practica3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello1");
+        System.out.println("Hello world!");
+        HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     }
     
 }
